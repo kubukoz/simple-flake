7444b42971012ce886ad9b8174a0ccb5b5b10ed9
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-03 03:44:16 +0100

    add shell hook

--- flake.nix
+++ flake.nix
@@ -1,1 +1,12 @@
-{ outputs = { self, nixpkgs, ... } : { packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.cowsay; } ;}
+{
+  outputs = { self, nixpkgs, ... }: {
+    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.cowsay;
+    devShells.aarch64-darwin.default = let pkgs = import nixpkgs { system = "aarch64-darwin"; }; in
+      pkgs.mkShell {
+        packages = [ pkgs.hello ];
+        shellHook = ''
+          FOO=world ${./shell-hook.sh}
+        '';
+      };
+  };
+}
