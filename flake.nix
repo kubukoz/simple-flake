f18e62fa892b521a15a98f56e69d5a7ef6e572fb
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-24 03:27:20 +0100

    crap

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
   outputs = { self, nixpkgs, ... }: {
     packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.cowsay;
     devShells.aarch64-darwin.default = let pkgs = import nixpkgs { system = "aarch64-darwin"; }; in
       pkgs.mkShell {
-        packages = [ pkgs.hello ];
-        shellHook = ''
-          FOO=world ${./shell-hook.sh}
-        '';
+        buildInputs = [ pkgs.jdk8 ];
       };
   };
 }
