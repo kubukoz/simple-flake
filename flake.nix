877d75286164a4a4c25faa020404ae1c9e5c9a2f
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-24 03:27:38 +0100

    changes

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.cowsay;
     devShells.aarch64-darwin.default = let pkgs = import nixpkgs { system = "aarch64-darwin"; }; in
       pkgs.mkShell {
-        buildInputs = [ pkgs.jdk8 ];
+        buildInputs = [ pkgs.jdk8 pkgs.coursier ];
       };
   };
 }
